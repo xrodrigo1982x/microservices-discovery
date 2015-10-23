ddac4781e9d7e59b986de8d9a1e1e9bbfd845a57
Rodrigo Santos <rodrigo.santos@b2wdigital.com>
2015-10-23 16:48:50 -0200

    separado do config

--- src/main/java/com/tweet/config/DiscoveryMain.java
+++ src/main/java/com/tweet/config/DiscoveryMain.java
@@ -1,21 +1,18 @@
 package com.tweet.config;
 
 import com.thetransactioncompany.cors.CORSFilter;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
 @EnableEurekaServer
-public class ConfigMain {
+public class DiscoveryMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigMain.class, args);
+        SpringApplication.run(DiscoveryMain.class, args);
     }
 
     @Bean
